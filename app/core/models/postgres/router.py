b22bfe03d3d78e99fc2b2420edc78eb475d4fee9
Sodez <korotchenkov245@gmail.com>
2025-02-03 19:30:16 +0300

    Убрал позор prefix

--- app/core/models/postgres/router.py
+++ app/core/models/postgres/router.py
@@ -2,16 +2,12 @@ from fastapi import APIRouter
 
 from sqlalchemy import select
 
-from core.database.postgres.database import async_session
+from app.core.database.postgres.database import session
 
-from models import DocumentBase
 
-router = APIRouter(prefix="/документы", tags=["Документы"])
+
+router = APIRouter(tags=["Документы"])
 
 @router.get("/documents", summary="Получить все документы")
-async def get_all_documents():
-    async with async_session() as session:
-        query = select(DocumentBase)
-        result = await session.execute(query)
-        documents = result.scalars().all()
-        return documents
+def get_all_documents():
+        pass
